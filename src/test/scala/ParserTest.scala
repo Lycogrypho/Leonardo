b87cc132c93a067a69a05b86dfdf238a7c5ed006
Cosimo Attanasi <c.attanasi@grypho.it>
2023-05-03 12:59:09 +0200

    updated tests
    Split previous test in two:
    - parsing tests (from string to internal objects)
    - representation tests (from internal object to String)
    
    Signed-off-by: Cosimo Attanasi <c.attanasi@grypho.it>

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -32,26 +32,6 @@ class ParserTest extends AnyFlatSpec with BeforeAndAfter
     ("-3k",                """(-1.0 * (3.0 * k))""",         Product(_Number(-1), Product(_Number(3.0), _Variable("k")))),
     ("sin(a)cos(b)",       """(sin(a) * cos(b))""",          Product(Sin(_Variable("a")), Cos(_Variable("b"))))
   )
-  //List( //("1",                  "test check"),
-  //    ("1",                  """((None~(1~None))~None)"""),
-  //    ("1 + 2",              """((None~(1~None))~Some((+~(2~None))))"""),
-  //    ("(1 + 9.2)",          """((None~((((~((None~(1~None))~Some((+~(9.2~None)))))~))~None))~None)"""),
-  //    ("3*3E-5",             """((None~(3~Some((*~3E-5))))~None)"""),
-  //    ("exp(1)",             """((None~((((exp~()~((None~(1~None))~None))~))~None))~None)"""),
-  //    ("sin(a)",             """((None~((((sin~()~((None~(a~None))~None))~))~None))~None)"""),
-  //    ("tg(x + 2)",          """((None~((((tg~()~((None~(x~None))~Some((+~(2~None)))))~))~None))~None)"""),
-  //    ("tg(x + log(2))",     """((None~((((tg~()~((None~(x~None))~Some((+~((((log~()~((None~(2~None))~None))~))~None)))))~))~None))~None)"""),
-  //    ("1* exp(x)",          """((None~(1~Some((*~(((exp~()~((None~(x~None))~None))~))))))~None)"""),
-  //    ("exp(cos(a))",        """((None~((((exp~()~((None~((((cos~()~((None~(a~None))~None))~))~None))~None))~))~None))~None)"""),
-  //    ("3a",                 """((None~(3~Some((~a))))~None)"""),
-  //    ("3sin(a)",            """((None~(3~Some((~(((sin~()~((None~(a~None))~None))~))))))~None)"""),
-  //    ("derive(cos(3x), x)", """((None~(((((derive(~((None~((((cos~()~((None~(3~Some((~x))))~None))~))~None))~None))~,)~x)~))~None))~None)"""),
-  //    ("-2",                 """((Some(-)~(2~None))~None)"""),
-  //    ("+3sin(-a)",          """((Some(+)~(3~Some((~(((sin~()~((Some(-)~(a~None))~None))~))))))~None)"""),
-  //    ("-3k",                """((Some(-)~(3~Some((~k))))~None)"""),
-  //    ("sin(a)cos(b)",       """((None~((((sin~()~((None~(a~None))~None))~))~Some((~(((cos~()~((None~(b~None))~None))~))))))~None)""")
-  //  )
-
 
   for (s <- expressionStrings)
   {
